f208f5b2a3f8abef0bdf0f3ea8ce6a4fc4c0b386
yasu0406 <yasucom4646@gmail.com>
2019-05-29 16:35:11 -0700

    Update App.js

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class App extends Component {
     render() {
         return(
-            <div>TEST</div>
+            <div>TEST2</div>
         );
     }
 }
