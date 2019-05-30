b8e9d5ffa7eaebb2e9c0ab7a01acc209bc8c57e5
yasu0406 <yasucom4646@gmail.com>
2019-05-30 13:01:43 -0700

    Update App.js

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,11 +1,19 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+import Header from './Header';
+import Slider from './Slider';
+import Home from './Home';
 
 class App extends Component {
-    render() {
-        return(
-            <div>TEST2</div>
-        );
-    }
+	render() {
+		return (
+			<BrowserRouter>
+				<Header />
+				<Slider />
+				<Route path="/" exact component={Home} />
+			</BrowserRouter>
+		);
+	}
 }
 
 export default App;
