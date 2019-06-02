14814d2ca8d7f0069e5c0e740cbcb10747834498
yasu0406 <yasucom4646@gmail.com>
2019-06-02 04:44:17 -0700

    Created login-page

--- addmin/src/components/App.js
+++ addmin/src/components/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Route, Link } from 'react-router-dom';
 
+import '../styles/common.scss';
+import '../styles/style.scss';
 import Header from './Header'
 import Login from './Login';
 
