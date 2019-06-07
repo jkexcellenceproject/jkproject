32e3bc20f297a7c4c0a102eed3272c671f7a5c35
yasu0406 <yasucom4646@gmail.com>
2019-06-07 16:32:08 -0700

    Added body-parser

--- server/index.js
+++ server/index.js
@@ -1,14 +1,16 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const mongodb = require('mongodb');
 const cookieSession = require('cookie-session');
 const passport = require('passport');
+bodyParser = require('body-parser')
 const keys = require('./config/dev');
 require('./modals/Blog');
 
 mongoose.connect(keys.mongoURI);
 
 const app = express();
+
+app.use(bodyParser.json())
 
 require('./routes/blogsRoutes')(app); 
 
