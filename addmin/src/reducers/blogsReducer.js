f5c417a0148854eb6d864b60d696ec4a64b087b7
yasu0406 <yasucom4646@gmail.com>
2019-06-07 16:31:43 -0700

    Added EDIT_BLOG for edit blog page

--- addmin/src/reducers/blogsReducer.js
+++ addmin/src/reducers/blogsReducer.js
@@ -1,10 +1,12 @@
 import _ from 'lodash';
-import { FETCH_BLOGS } from '../actions/types';
+import { FETCH_BLOGS, EDIT_BLOG } from '../actions/types';
 
-export default function(state = null, action) {
+export default function(state = {}, action) {
     switch(action.type) {
         case FETCH_BLOGS:
             return { ...state, ..._.mapKeys(action.payload, 'id') };
+         case EDIT_BLOG:
+             return {state: action.payload};   
         default:
             return state;
     }
