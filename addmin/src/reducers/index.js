6914ad08fbf674df9bc95163ec5a5b4e0435f397
yasu0406 <yasucom4646@gmail.com>
2019-06-14 14:38:34 -0700

    Added reduxForm state

--- addmin/src/reducers/index.js
+++ addmin/src/reducers/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux';
 import blogsReducer from './blogsReducer';
+import { reducer as reduxForm } from 'redux-form';
 
 export default combineReducers({
-    blogs: blogsReducer
+    blogs: blogsReducer,
+    form: reduxForm
 });
