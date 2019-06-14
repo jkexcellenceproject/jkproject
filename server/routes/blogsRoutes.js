30e17a4a8d044de6c09f3329071e445535b55232
yasu0406 <yasucom4646@gmail.com>
2019-06-14 14:39:55 -0700

    Added post of blog submit

--- server/routes/blogsRoutes.js
+++ server/routes/blogsRoutes.js
@@ -1,4 +1,4 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 const Blogs = mongoose.model('blogs');
 module.exports = app => {
     app.get('/api/blogs', (request, response) => {
@@ -9,10 +9,32 @@ module.exports = app => {
   })
 
   app.post('/api/blog/edit/:id', (request, response) => {
-    Blogs.find({"id":request.body.id}, (err, blogs) => {  
+    console.log(request.body.id);
+    Blogs.find({'id':request.body.id}, (err, blogs) => {  
       if (err) response.status(500).send()
       else response.status(200).send(blogs)
-      console.log(blogs);
+      console.log(response.body);
     })
+  })
+
+  app.post('/api/blog/submit', (request, response) => {
+    var id = 0;
+    const {title, content} = request.body.values;
+    Blogs.find({}).sort([['id', -1]]).limit(1).exec(function(err, blogs) { 
+      id = blogs[0].id + 1;
+      const blog = new Blogs({
+        id, 
+        title,
+        content
+      });
+      try{
+        blog.save();
+        Blogs.find({}, (err, blogs) => {  
+          response.status(200).send(blogs)
+        })
+        }catch(err) {
+          response.status(500).send()
+      } 
+     });
   })
 };
