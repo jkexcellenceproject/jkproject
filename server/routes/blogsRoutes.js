9200d5e68d231bc043a731c9322d5268ac7de70a
yasu0406 <yasucom4646@gmail.com>
2019-06-17 21:48:10 -0700

    Added Update and Delete

--- server/routes/blogsRoutes.js
+++ server/routes/blogsRoutes.js
@@ -1,21 +1,25 @@
 const mongoose = require('mongoose');
 const Blogs = mongoose.model('blogs');
+
 module.exports = app => {
-    app.get('/api/blogs', (request, response) => {
-        Blogs.find({}, (err, blogs) => {  
-        if (err) response.status(500).send()
-        else response.status(200).send(blogs)
-    })
+
+  // Find all for the Blogs page
+  app.get('/api/blogs', (request, response) => {
+      Blogs.find({}, (err, blogs) => {  
+      if (err) response.status(500).send()
+      else response.status(200).send(blogs)
+      })
   })
 
+  // Find one of the blog on the BlogEdit page
   app.post('/api/blog/edit/:id', (request, response) => {
-    console.log(request.body.id);
     Blogs.find({'id':request.body.id}, (err, blogs) => {  
       if (err) response.status(500).send()
       else response.status(200).send(blogs)
     })
   })
 
+  // Add new blog
   app.post('/api/blog/submit', (request, response) => {
     var id = 0;
     const {title, content} = request.body.values;
@@ -27,13 +31,34 @@ module.exports = app => {
         content
       });
       try{
-        blog.save();
-        Blogs.find({}, (err, blogs) => {  
-          response.status(200).send(blogs)
-        })
-        }catch(err) {
-          response.status(500).send()
-      } 
-     });
+          blog.save();
+          Blogs.find({}, (err, blogs) => {  
+              response.status(200).send(blogs)
+          })
+        } catch(err) {
+            response.status(500).send()
+        } 
+     })
   })
-};
+    
+  // Update the blog 
+  app.post('/api/blog/editSubmit', (request, response) => {
+    console.log(request.body.values);
+    const {title, content} = request.body.values;
+    Blogs.findOneAndUpdate({'id':request.body.id},{'title':title,'content':content}, (err, blogs) => {
+      if(err) response.status(500).send()
+      else response.status(200).send(blogs)
+      console.log(blogs);
+    })
+   }) 
+
+
+   // Delete the blog
+   app.post('/api/blog/delete', (request, response) => {
+     console.log(request.body.id);
+     Blogs.deleteOne({'id': request.body.id }, (err, blogs) => {
+       if(err) response.status(500).send
+       else response.status(200).send(blogs)
+     })
+   })
+}
