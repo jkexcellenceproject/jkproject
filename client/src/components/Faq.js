7160b0fa7c3299c8ddf023757ad7839598a9cb68
Kano Flordelis Gomez <ka958376@gmail.com>
2019-06-19 13:25:41 -0700

    Added some css to Faq.js

--- client/src/components/Faq.js
+++ client/src/components/Faq.js
@@ -35,106 +35,255 @@ class Faq extends React.Component {
 						</div>
 					</div>
 				</div>
-				<div className="ui fluid container content-padding">
+				<div className="ui fluid container blueGreen content-padding">
 					<div className="ui container">
-						<Accordion styled>
-							<Accordion.Title
-								active={activeIndex === 0}
-								index={0}
-								onClick={this.handleClick}
-							>
-								<Icon name="dropdown" />
-								What is a dog?
-							</Accordion.Title>
-							<Accordion.Content active={activeIndex === 0}>
-								<p>
-									A dog is a type of domesticated animal. Known for its loyalty
-									and faithfulness, it can be found as a welcome guest in many
-									households across the world.
-								</p>
-							</Accordion.Content>
+						<div className="ui grid">
+							<div className="row">
+								<div className="center aligned column">
+									<h2 className="ui header white-text">FAQ's</h2>
+								</div>
+							</div>
+							<div className="two column row">
+								<div className="column">
+									<Accordion styled>
+										<Accordion.Title
+											active={activeIndex === 0}
+											index={0}
+											onClick={this.handleClick}
+										>
+											<Icon name="dropdown" />
+											What is a dog?
+										</Accordion.Title>
+										<Accordion.Content active={activeIndex === 0}>
+											<p>
+												A dog is a type of domesticated animal. Known for its
+												loyalty and faithfulness, it can be found as a welcome
+												guest in many households across the world.
+											</p>
+										</Accordion.Content>
 
-							<Accordion.Title
-								active={activeIndex === 1}
-								index={1}
-								onClick={this.handleClick}
-							>
-								<Icon name="dropdown" />
-								What kinds of dogs are there?
-							</Accordion.Title>
-							<Accordion.Content active={activeIndex === 1}>
-								<p>
-									There are many breeds of dogs. Each breed varies in size and
-									temperament. Owners often select a breed of dog that they find
-									to be compatible with their own lifestyle and desires from a
-									companion.
-								</p>
-							</Accordion.Content>
+										<Accordion.Title
+											active={activeIndex === 1}
+											index={1}
+											onClick={this.handleClick}
+										>
+											<Icon name="dropdown" />
+											What kinds of dogs are there?
+										</Accordion.Title>
+										<Accordion.Content active={activeIndex === 1}>
+											<p>
+												There are many breeds of dogs. Each breed varies in size
+												and temperament. Owners often select a breed of dog that
+												they find to be compatible with their own lifestyle and
+												desires from a companion.
+											</p>
+										</Accordion.Content>
 
-							<Accordion.Title
-								active={activeIndex === 2}
-								index={2}
-								onClick={this.handleClick}
-							>
-								<Icon name="dropdown" />
-								How do you acquire a dog?
-							</Accordion.Title>
-							<Accordion.Content active={activeIndex === 2}>
-								<p>
-									Three common ways for a prospective owner to acquire a dog is
-									from pet shops, private owners, or shelters.
-								</p>
-								<p>
-									A pet shop may be the most convenient way to buy a dog. Buying
-									a dog from a private owner allows you to assess the pedigree
-									and upbringing of your dog before choosing to take it home.
-									Lastly, finding your dog from a shelter, helps give a good
-									home to a dog who may not find one so readily.
-								</p>
-							</Accordion.Content>
-						</Accordion>
-						{/* <div className="ui styled accordion">
-							<div className="title">
-								<i className="dropdown icon" />
-								What is a dog?
+										<Accordion.Title
+											active={activeIndex === 2}
+											index={2}
+											onClick={this.handleClick}
+										>
+											<Icon name="dropdown" />
+											How do you acquire a dog?
+										</Accordion.Title>
+										<Accordion.Content active={activeIndex === 2}>
+											<p>
+												Three common ways for a prospective owner to acquire a
+												dog is from pet shops, private owners, or shelters.
+											</p>
+											<p>
+												A pet shop may be the most convenient way to buy a dog.
+												Buying a dog from a private owner allows you to assess
+												the pedigree and upbringing of your dog before choosing
+												to take it home. Lastly, finding your dog from a
+												shelter, helps give a good home to a dog who may not
+												find one so readily.
+											</p>
+										</Accordion.Content>
+									</Accordion>
+								</div>
+								<div className="column">
+									<Accordion styled>
+										<Accordion.Title
+											active={activeIndex === 0}
+											index={0}
+											onClick={this.handleClick}
+										>
+											<Icon name="dropdown" />
+											What is a dog?
+										</Accordion.Title>
+										<Accordion.Content active={activeIndex === 0}>
+											<p>
+												A dog is a type of domesticated animal. Known for its
+												loyalty and faithfulness, it can be found as a welcome
+												guest in many households across the world.
+											</p>
+										</Accordion.Content>
+
+										<Accordion.Title
+											active={activeIndex === 1}
+											index={1}
+											onClick={this.handleClick}
+										>
+											<Icon name="dropdown" />
+											What kinds of dogs are there?
+										</Accordion.Title>
+										<Accordion.Content active={activeIndex === 1}>
+											<p>
+												There are many breeds of dogs. Each breed varies in size
+												and temperament. Owners often select a breed of dog that
+												they find to be compatible with their own lifestyle and
+												desires from a companion.
+											</p>
+										</Accordion.Content>
+
+										<Accordion.Title
+											active={activeIndex === 2}
+											index={2}
+											onClick={this.handleClick}
+										>
+											<Icon name="dropdown" />
+											How do you acquire a dog?
+										</Accordion.Title>
+										<Accordion.Content active={activeIndex === 2}>
+											<p>
+												Three common ways for a prospective owner to acquire a
+												dog is from pet shops, private owners, or shelters.
+											</p>
+											<p>
+												A pet shop may be the most convenient way to buy a dog.
+												Buying a dog from a private owner allows you to assess
+												the pedigree and upbringing of your dog before choosing
+												to take it home. Lastly, finding your dog from a
+												shelter, helps give a good home to a dog who may not
+												find one so readily.
+											</p>
+										</Accordion.Content>
+									</Accordion>
+								</div>
 							</div>
-							<div className="content">
-								<p className="transition hidden">
-									A dog is a type of domesticated animal. Known for its loyalty
-									and faithfulness, it can be found as a welcome guest in many
-									households across the world.
-								</p>
+							<div className="two column row">
+								<div className="column">
+									<Accordion styled>
+										<Accordion.Title
+											active={activeIndex === 0}
+											index={0}
+											onClick={this.handleClick}
+										>
+											<Icon name="dropdown" />
+											What is a dog?
+										</Accordion.Title>
+										<Accordion.Content active={activeIndex === 0}>
+											<p>
+												A dog is a type of domesticated animal. Known for its
+												loyalty and faithfulness, it can be found as a welcome
+												guest in many households across the world.
+											</p>
+										</Accordion.Content>
+
+										<Accordion.Title
+											active={activeIndex === 1}
+											index={1}
+											onClick={this.handleClick}
+										>
+											<Icon name="dropdown" />
+											What kinds of dogs are there?
+										</Accordion.Title>
+										<Accordion.Content active={activeIndex === 1}>
+											<p>
+												There are many breeds of dogs. Each breed varies in size
+												and temperament. Owners often select a breed of dog that
+												they find to be compatible with their own lifestyle and
+												desires from a companion.
+											</p>
+										</Accordion.Content>
+
+										<Accordion.Title
+											active={activeIndex === 2}
+											index={2}
+											onClick={this.handleClick}
+										>
+											<Icon name="dropdown" />
+											How do you acquire a dog?
+										</Accordion.Title>
+										<Accordion.Content active={activeIndex === 2}>
+											<p>
+												Three common ways for a prospective owner to acquire a
+												dog is from pet shops, private owners, or shelters.
+											</p>
+											<p>
+												A pet shop may be the most convenient way to buy a dog.
+												Buying a dog from a private owner allows you to assess
+												the pedigree and upbringing of your dog before choosing
+												to take it home. Lastly, finding your dog from a
+												shelter, helps give a good home to a dog who may not
+												find one so readily.
+											</p>
+										</Accordion.Content>
+									</Accordion>
+								</div>
+								<div className="column">
+									<Accordion styled>
+										<Accordion.Title
+											active={activeIndex === 0}
+											index={0}
+											onClick={this.handleClick}
+										>
+											<Icon name="dropdown" />
+											What is a dog?
+										</Accordion.Title>
+										<Accordion.Content active={activeIndex === 0}>
+											<p>
+												A dog is a type of domesticated animal. Known for its
+												loyalty and faithfulness, it can be found as a welcome
+												guest in many households across the world.
+											</p>
+										</Accordion.Content>
+
+										<Accordion.Title
+											active={activeIndex === 1}
+											index={1}
+											onClick={this.handleClick}
+										>
+											<Icon name="dropdown" />
+											What kinds of dogs are there?
+										</Accordion.Title>
+										<Accordion.Content active={activeIndex === 1}>
+											<p>
+												There are many breeds of dogs. Each breed varies in size
+												and temperament. Owners often select a breed of dog that
+												they find to be compatible with their own lifestyle and
+												desires from a companion.
+											</p>
+										</Accordion.Content>
+
+										<Accordion.Title
+											active={activeIndex === 2}
+											index={2}
+											onClick={this.handleClick}
+										>
+											<Icon name="dropdown" />
+											How do you acquire a dog?
+										</Accordion.Title>
+										<Accordion.Content active={activeIndex === 2}>
+											<p>
+												Three common ways for a prospective owner to acquire a
+												dog is from pet shops, private owners, or shelters.
+											</p>
+											<p>
+												A pet shop may be the most convenient way to buy a dog.
+												Buying a dog from a private owner allows you to assess
+												the pedigree and upbringing of your dog before choosing
+												to take it home. Lastly, finding your dog from a
+												shelter, helps give a good home to a dog who may not
+												find one so readily.
+											</p>
+										</Accordion.Content>
+									</Accordion>
+								</div>
 							</div>
-							<div className="title">
-								<i className="dropdown icon" />
-								What kinds of dogs are there?
-							</div>
-							<div className="content">
-								<p className="transition hidden">
-									There are many breeds of dogs. Each breed varies in size and
-									temperament. Owners often select a breed of dog that they find
-									to be compatible with their own lifestyle and desires from a
-									companion.
-								</p>
-							</div>
-							<div className="title active">
-								<i className="dropdown icon" />
-								How do you acquire a dog?
-							</div>
-							<div className="content active">
-								<p className="transition visible">
-									Three common ways for a prospective owner to acquire a dog is
-									from pet shops, private owners, or shelters.
-								</p>
-								<p className="transition visible">
-									A pet shop may be the most convenient way to buy a dog. Buying
-									a dog from a private owner allows you to assess the pedigree
-									and upbringing of your dog before choosing to take it home.
-									Lastly, finding your dog from a shelter, helps give a good
-									home to a dog who may not find one so readily.
-								</p>
-							</div>
-						</div> */}
+						</div>
 					</div>
 				</div>
 			</div>
