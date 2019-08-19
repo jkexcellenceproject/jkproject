9b45195a9cac3cff05b8a2b48558fc006304f3e2
Kano Flordelis Gomez <ka958376@gmail.com>
2019-08-19 15:51:38 -0700

    added faq page folder
    faq folder includes Faq.js, Accordion.js, AccordionParts.js and faq.scss

---
+++ client/src/components/faq/Accordion.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import AccordionParts from './AccordionParts';
+
+const Accordion = props => {
+	// console.log(props);
+	return (
+		<div className="accordion">
+			<h4 className="faq-section-title">{props.title}</h4>
+			<AccordionParts
+				title="Question 1"
+				content="Good Morning!"
+				idForLabel={`${props.section}`}
+			/>
+			<AccordionParts
+				title="Question 2"
+				content="Good Evening!"
+				idForLabel={`${props.section}-1`}
+			/>
+			<AccordionParts
+				title="Question 3"
+				content="Good Night!"
+				idForLabel={`${props.section}-2`}
+			/>
+		</div>
+	);
+};
+
+export default Accordion;
